41e9867c0bb9127f35d977893f5986a389d2bfb5
ammarzafar4372 <ammarzafar4372@gmail.com>
2024-03-11 23:06:06 +0500

    Day 1 part 2 is done

--- Day 1/part_1.py
+++ Day 1/part_1.py
@@ -2,30 +2,24 @@ def main():
     book_path = "input.txt"
     text = get_text(book_path)
     line_list = get_line_list(text)
-    number_list = []
+    sum = 0
     for line in line_list:
-        number = get_num_from_string(line)
-        number_list.append(number)
-    sum_of_number_list = sum_list(number_list)
-    print(sum_of_number_list)
+        sum += get_num_from_string(line)
+    print(sum)
     return
 
-def sum_list(number_list: list) -> int:
-    sum = 0
-    for number in number_list:
-        sum += number
-    return sum
-
 def get_num_from_string(line: str) -> int:
-    digit = []
     number = 0
-    for letter in line:
-        if letter.isdigit():
-            digit.append(int(letter))
-    try :
-        number = digit[0]*10 + digit[len(digit)-1]
-    except IndexError :
-        number = digit[0]*10 + digit[0]
+    digits = []
+    for char in line:
+        if char.isdigit():
+            digits.append(int(char))
+            break   
+    for char in line[::-1]:
+        if char.isdigit():
+            digits.append(int(char))
+            break 
+    number = digits[0] *10 + digits[1]
     return number
 
 def get_line_list(text: str) -> list:
--- Day 1/part_2.py
+++ Day 1/part_2.py
@@ -2,24 +2,14 @@ def main():
     book_path = "input.txt"
     text = get_text(book_path)
     line_list = get_line_list(text)
-    number_list = []
+    sum = 0
     for line in line_list:
-        number = get_num_from_string(line)
-        number_list.append(number)
-    sum_of_number_list = sum_list(number_list)
-    print(sum_of_number_list)
+        sum += get_num_from_string(line)
+    print(sum)
     return
 
-def sum_list(number_list: list) -> int:
-    sum = 0
-    for number in number_list:
-        sum += number
-    return sum
-
-def get_num_from_string(line: str) -> int:
-    digit = []
-    number = 0
-    num_dict = {
+def str_to_int(digit: str) -> int:
+    conv = {
         "one": 1,
         "two": 2,
         "three": 3,
@@ -30,28 +20,58 @@ def get_num_from_string(line: str) -> int:
         "eight": 8,
         "nine": 9,
     }
-    spelled_digit = ""
-    for letter in line:
-        if letter.isdigit():
-            digit.append(int(letter))
-        else:
-            spelled_digit += letter
-        if spelled_digit in num_dict:
-            digit.append(num_dict[spelled_digit])
-            spelled_digit=""
-    try :
-        number = digit[0]*10 + digit[len(digit)-1]
-    except IndexError :
-        number = digit[0]*10 + digit[0]
-    print(digit,number)
+    return conv[digit]
+
+def get_num_from_string(line: str) -> int:
+    number = 0
+    possible_digits = {
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9"
+    }
+    first_digit = ""
+    last_digit = ""
+    for char in line:
+        first_digit += char
+        for digit in possible_digits:
+            if first_digit.find(digit) != -1:
+                if first_digit.isalpha():
+                    digit=str_to_int(digit)
+                number += int(digit)
+        if number > 0:
+            break   
+    for char in line[::-1]:
+        last_digit = char + last_digit
+        for digit in possible_digits:
+            if last_digit.find(digit) != -1:
+                if last_digit.isalpha():
+                    digit=str_to_int(digit)
+                number = number * 10 + int(digit)
+        if number > 10:
+            break   
     return number
 
 def get_line_list(text: str) -> list:
-    words = text.split('\n')
-    return words
+    lines = text.split('\n')
+    return lines
 
 def get_text(file: str) -> str:
     with open(file) as f:
-       return f.read()
+        return f.read()
         
 main()
